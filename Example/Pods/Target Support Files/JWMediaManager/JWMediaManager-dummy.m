af2eb7fc5fd326923981da1426ff9bfc0a407b07
Jason Wang <jianxiongwang@Jianxiongs-MacBook-Pro.local>
2017-02-16 13:42:52 -0500

    Create bare project via pod

---
+++ Example/Pods/Target Support Files/JWMediaManager/JWMediaManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JWMediaManager : NSObject
+@end
+@implementation PodsDummy_JWMediaManager
+@end
---
+++ Example/Pods/Target Support Files/Pods-JWMediaManager_Example/Pods-JWMediaManager_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JWMediaManager_Example : NSObject
+@end
+@implementation PodsDummy_Pods_JWMediaManager_Example
+@end
